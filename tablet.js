d2f1b0ebaef03183c48dacfa61ae9a7abd9adee4
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-10-11 14:41:50 +0300

    Updated

--- tablet.js
+++ tablet.js
@@ -4,33 +4,9 @@ body {
     font-size: 16px;
 }
 
-.header,
-.offer,
-.our-team,
-.motivation,
-.blog-news,
-footer {
+.header {
     width: 90%;
     padding: 20px;
-}
-.blog-news ul{
-    flex-direction: column;
-}
-.blog-news ul .news {
-    list-style: none outside;
-    display: flex;
-    text-align: center;
-    flex: 0 0 35%;
-    box-shadow: 0 0.3rem 1rem 0.3rem rgba(0, 0, 0, 0.3);
-    margin-top: 10px;
-}
-.motivation ul{
-    flex-direction: column;
-}
-
-.img img {
-    max-width: 100%;
-    height: auto;
 }
 `;
 
